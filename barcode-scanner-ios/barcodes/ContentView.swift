33b4fd9ad3497152b42b26424ed4c9d3ef75aef9
Sam Lijin <sam@boundaryml.com>
2025-12-10 19:47:55 -0800

    barcode-scanner-ios: scaffolding

---
+++ barcode-scanner-ios/barcodes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  barcodes
+//
+//  Created by Sam on 12/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ barcode-scanner-ios/barcodes/barcodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodesApp.swift
+//  barcodes
+//
+//  Created by Sam on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct barcodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
